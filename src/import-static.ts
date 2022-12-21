6a8fad07d81862bed99a56b21b3a640192e92231
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-12-20 22:39:31 -0500

    docs(regex): add `STATIC_IMPORT_REGEX` example
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/import-static.ts
+++ src/import-static.ts
@@ -7,6 +7,107 @@
  * Static `import` statement regex. Ignores matches in comments.
  *
  * @see https://regex101.com/r/wlYQUN
+ * @see https://github.com/tc39/proposal-import-assertions#import-statements
+ *
+ * @example
+ *  import { STATIC_IMPORT_REGEX } from '@flex-development/import-regex'
+ *  import { dedent } from 'ts-dedent'
+ *
+ *  const code: string = dedent`
+ *    import { defineBuildConfig } from '@flex-development/mkbuild'
+ *    import type {
+ *      Join,
+ *      Nullable,
+ *      Opaque,
+ *      Simplify
+ *    } from '@flex-development/tutils'
+ *    import * as color from 'colorette'
+ *    import consola from 'consola'
+ *    import tsconfig from './tsconfig.json' assert { type: 'json' }
+ *  `
+ *
+ *  const print = (matches: IterableIterator<RegExpMatchArray>): void => {
+ *    console.debug([...matches].map(match => omit(match, ['input'])))
+ *  }
+ *
+ *  print(code.matchAll(STATIC_IMPORT_REGEX))
+ *  // [
+ *  //   {
+ *  //     '0': "import { defineBuildConfig } from '@flex-development/mkbuild'",
+ *  //     '1': undefined,
+ *  //     '2': '{ defineBuildConfig }',
+ *  //     '3': '@flex-development/mkbuild',
+ *  //     '4': undefined,
+ *  //     index: 0,
+ *  //     groups: [Object: null prototype] {
+ *  //       type: undefined,
+ *  //       imports: '{ defineBuildConfig }',
+ *  //       specifier: '@flex-development/mkbuild',
+ *  //       assertion: undefined
+ *  //     }
+ *  //   },
+ *  //   {
+ *  //     '0': 'import type {\n' +
+ *  //       '  Join,\n' +
+ *  //       '  Nullable,\n' +
+ *  //       '  Opaque,\n' +
+ *  //       '  Simplify\n' +
+ *  //       "} from '@flex-development/tutils'",
+ *  //     '1': 'type',
+ *  //     '2': '{\n  Join,\n  Nullable,\n  Opaque,\n  Simplify\n}',
+ *  //     '3': '@flex-development/tutils',
+ *  //     '4': undefined,
+ *  //     index: 62,
+ *  //     groups: [Object: null prototype] {
+ *  //       type: 'type',
+ *  //       imports: '{\n  Join,\n  Nullable,\n  Opaque,\n  Simplify\n}',
+ *  //       specifier: '@flex-development/tutils',
+ *  //       assertion: undefined
+ *  //     }
+ *  //   },
+ *  //   {
+ *  //     '0': "import * as color from 'colorette'",
+ *  //     '1': undefined,
+ *  //     '2': '* as color',
+ *  //     '3': 'colorette',
+ *  //     '4': undefined,
+ *  //     index: 151,
+ *  //     groups: [Object: null prototype] {
+ *  //       type: undefined,
+ *  //       imports: '* as color',
+ *  //       specifier: 'colorette',
+ *  //       assertion: undefined
+ *  //     }
+ *  //   },
+ *  //   {
+ *  //     '0': "import consola from 'consola'",
+ *  //     '1': undefined,
+ *  //     '2': 'consola',
+ *  //     '3': 'consola',
+ *  //     '4': undefined,
+ *  //     index: 186,
+ *  //     groups: [Object: null prototype] {
+ *  //       type: undefined,
+ *  //       imports: 'consola',
+ *  //       specifier: 'consola',
+ *  //       assertion: undefined
+ *  //     }
+ *  //   },
+ *  //   {
+ *  //     '0': "import tsc from './tsconfig.json' assert { type: 'json' }",
+ *  //     '1': undefined,
+ *  //     '2': 'tsc',
+ *  //     '3': './tsconfig.json',
+ *  //     '4': "{ type: 'json' }",
+ *  //     index: 216,
+ *  //     groups: [Object: null prototype] {
+ *  //       type: undefined,
+ *  //       imports: 'tsc',
+ *  //       specifier: './tsconfig.json',
+ *  //       assertion: "{ type: 'json' }"
+ *  //     }
+ *  //   }
+ *  // ]
  *
  * @const {RegExp} STATIC_IMPORT_REGEX
  */
