7c1cda667f1178dc33681f862ed2ec51729a4b7f
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-12-20 22:38:42 -0500

    docs(regex): add `DYNAMIC_IMPORT_REGEX` example
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/import-dynamic.ts
+++ src/import-dynamic.ts
@@ -11,6 +11,67 @@
  * [1]: https://javascript.info/regexp-unicode
  *
  * @see https://regex101.com/r/PTPAvU
+ * @see https://github.com/tc39/proposal-import-assertions#dynamic-import
+ *
+ * @example
+ *  import { DYNAMIC_IMPORT_REGEX } from '@flex-development/import-regex'
+ *  import { dedent } from 'ts-dedent'
+ *
+ *  const code: string = dedent`
+ *    await import('./side-effect.mjs')
+ *    await import(specifier)
+ *    import('foo.json', { assert: { type: 'json' } })
+ *  `
+ *
+ *  const print = (matches: IterableIterator<RegExpMatchArray>): void => {
+ *    console.debug([...matches].map(match => omit(match, ['input'])))
+ *  }
+ *
+ *  print(code.matchAll(DYNAMIC_IMPORT_REGEX))
+ *  //  [
+ *  //   {
+ *  //     '0': "await import('./side-effect.mjs')",
+ *  //     '1': undefined,
+ *  //     '2': "import('./side-effect.mjs')",
+ *  //     '3': "'./side-effect.mjs'",
+ *  //     '4': undefined,
+ *  //     index: 0,
+ *  //     groups: [Object: null prototype] {
+ *  //       imports: undefined,
+ *  //       expression: "import('./side-effect.mjs')",
+ *  //       specifier: "'./side-effect.mjs'",
+ *  //       options: undefined
+ *  //     }
+ *  //   },
+ *  //   {
+ *  //     '0': 'await import(specifier)',
+ *  //     '1': undefined,
+ *  //     '2': 'import(specifier)',
+ *  //     '3': 'specifier',
+ *  //     '4': undefined,
+ *  //     index: 34,
+ *  //     groups: [Object: null prototype] {
+ *  //       imports: undefined,
+ *  //       expression: 'import(specifier)',
+ *  //       specifier: 'specifier',
+ *  //       options: undefined
+ *  //     }
+ *  //   },
+ *  //   {
+ *  //     '0': "import('foo.json', { assert: { type: 'json' } })",
+ *  //     '1': undefined,
+ *  //     '2': "import('foo.json', { assert: { type: 'json' } })",
+ *  //     '3': "'foo.json'",
+ *  //     '4': "{ assert: { type: 'json' } }",
+ *  //     index: 58,
+ *  //     groups: [Object: null prototype] {
+ *  //       imports: undefined,
+ *  //       expression: "import('foo.json', { assert: { type: 'json' } })",
+ *  //       specifier: "'foo.json'",
+ *  //       options: "{ assert: { type: 'json' } }"
+ *  //     }
+ *  //   }
+ *  // ]
  *
  * @const {RegExp} DYNAMIC_IMPORT_REGEX
  */
