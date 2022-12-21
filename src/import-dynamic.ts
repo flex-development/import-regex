ccfea7ca944c24b70c48c822fc6e40900f2648ab
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-12-20 21:30:05 -0500

    refactor(regex)!: [dynamic] capture options bag
    - groups.assertion -> groups.options
    - https://github.com/tc39/proposal-import-assertions#dynamic-import
    
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/import-dynamic.ts
+++ src/import-dynamic.ts
@@ -15,6 +15,6 @@
  * @const {RegExp} DYNAMIC_IMPORT_REGEX
  */
 const DYNAMIC_IMPORT_REGEX: RegExp =
-  /(?<=^|[\s;])(?:(?:const\s*|let\s*|var\s*)?(?<imports>(?:[$_\p{ID_Start}][$\u200C\u200D\p{ID_Continue}]*)|(?<=const\s*|let\s*|var\s*)(?:[\w\t\n\r "$'*,./:{}-]+?))\s*=\s*)?(?:await\s+)?(?<expression>import\((?<specifier>[\S\t\n\r]+?)(?:,\s*\{\s*assert:\s*(?<assertion>\{[\w\t\n\r "':]+?\})[\n )}]+)?\))(?<!(?:\/\/|\*).*)/gu
+  /(?<=^|[\s;])(?:(?:const\s*|let\s*|var\s*)?(?<imports>(?:[$_\p{ID_Start}][$\u200C\u200D\p{ID_Continue}]*)|(?<=const\s*|let\s*|var\s*)(?:[\w\t\n\r "$'*,./:{}-]+?))\s*=\s*)?(?:await\s+)?(?<expression>import\((?<specifier>[\S\t\n\r]+?)(?:,\s*(?<options>\{\s*assert:.+[\w\t\n\r "':]+\}\s*\}))?\))(?<!(?:\/\/|\*).*)/gu
 
 export default DYNAMIC_IMPORT_REGEX
